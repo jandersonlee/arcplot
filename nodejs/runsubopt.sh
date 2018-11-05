f4224713a86661751fac8eac67c3df0eab88d7f2
Jeff Anderson-Lee <jonah@eecs.berkeley.edu>
2018-11-05 12:36:01 -0800

    add port as arg to runsubopt.sh

--- nodejs/runsubopt.sh
+++ nodejs/runsubopt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 NODE=/usr/bin/node
+PORT=${1-8888}
 [ -x /usr/bin/nodejs ] && NODE=/usr/bin/nodejs
-nohup $NODE subopt.js </dev/null >runsubopt.log 2>&1 &
+nohup $NODE subopt.js $PORT </dev/null >runsubopt.log 2>&1 &
 sleep 1
 cat runsubopt.log
